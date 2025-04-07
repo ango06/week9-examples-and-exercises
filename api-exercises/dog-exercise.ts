808bfae15fb4c4eed50cd3b123e3bf353ed5ec89
Angela Ngo <ngoangela6@gmail.com>
2025-04-07 18:13:31 -0400

    finished example

--- api-exercises/dog-exercise.ts
+++ api-exercises/dog-exercise.ts
@@ -1,27 +1,39 @@
 interface DogBreeds {
    //fill in
+   [breed: string]: string[]
 }
 
 async function fetchDogBreeds(): Promise<DogBreeds> {
     const response = await fetch('https://dog.ceo/api/breeds/list/all');
+    const data = await response.json();
 
-    //complete function
+    return data.message as DogBreeds; // ensures type is DogBreeds
 }
 
 function filterBreeds(breeds: DogBreeds, breedName: string): string[] {
     const filteredBreeds: string[] = [];
 
     //complete function
+    for (const breed in breeds){
+        if (breed.toLowerCase().includes(breedName.toLowerCase())){
+            filteredBreeds.push(breed);
+        }
+    }
     
     return filteredBreeds;
 }
 
 async function main(){
     const breeds = await fetchDogBreeds();
-    const breedName = "";
+    //const breedName = "";
+    const breedName = "bulldog";
     const filteredBreeds = filterBreeds(breeds, breedName);
 
+    console.log(breeds);
     //print out the filtered breeds
+
+    // how to run file
+    console.log(filteredBreeds)
 }
 
 main();
