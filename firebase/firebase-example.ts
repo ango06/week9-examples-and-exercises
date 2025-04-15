49bd5b2d19cbf2f75b893a6d34fc614b3ebeb902
Angela Ngo <ngoangela6@gmail.com>
2025-04-15 18:03:32 -0400

    finished example

--- firebase/firebase-example.ts
+++ firebase/firebase-example.ts
@@ -1,4 +1,4 @@
-import { collection, DocumentReference, getDocs, query, where } from "firebase/firestore";
+import { addDoc, arrayUnion, collection, deleteDoc, DocumentReference, getDoc, getDocs, query, updateDoc, where } from "firebase/firestore";
 import { Firestore } from 'firebase/firestore';
 
 import { db } from "./config/firebase";
@@ -24,52 +24,109 @@ async function getUser(db: Firestore, name: string){
     return snapshot.docs[0].ref;
 }
 
+//add new user to user collection
 async function addUser(db: Firestore, user: User){
-    //add new user to user collection
+    const usersCol = collection(db, "users");
+    await addDoc(usersCol, {
+        ...user,
+        order: user.orders ?? []
+    })
 }
 
-async function removeUser(db: Firestore, user: DocumentReference){
-    //remove user from the user collection
+//remove user from the user collection
+async function removeUser(user: DocumentReference){ // just need a ref to data doc, pointer
+    await deleteDoc(user); // could just delete directly, like below
 }
 
+//get all documents in food collection
 async function getFoods(db: Firestore){
-    //get all documents in food collection
+    const foodCollection = collection(db, 'food');
+    const foodSnapshot = await getDocs(foodCollection);
+    const foodList = foodSnapshot.docs.map(doc => doc.data());
+    return foodList;
 }
 
+//get DocumentReference to a food
 async function getFood(db: Firestore, name: string){
-    //get DocumentReference to a food
+    const foodCollection = collection(db, "food");
+    const q = query(foodCollection, where("name", "==", name));
+    const snapshot = await getDocs(q); // returns query snapshot, what matches query
+
+    if (snapshot.empty) {
+        console.warn(`No food found with name: ${name}`);
+        return null;
+    }
+
+    return snapshot.docs[0].ref; // return pointer to the first found 
 }
 
+//add new food to food collection
 async function addFood(db: Firestore, food: Food){
-    //add new food to food collection
+    const foodCollection = collection(db, "food");
+    await addDoc(foodCollection, food); // would return doc ref or promise doc ref if you want to use it
 }
 
-async function addOrder(db: Firestore, userName: DocumentReference, order: DocumentReference){
-    //add an existing food to existing 
+//add an existing food to existing 
+async function addOrder(userName: DocumentReference, order: DocumentReference){
+    await updateDoc(userName, {
+        order: arrayUnion(order) // if it was order: arrayUnion(order), it creates a new field?
+    });
 }
 
 async function main(){
     //retrieve users
-    const data = await getUsers(db)
-    console.log(data)
+    const userList = await getUsers(db)
+    console.log(userList)
 
+    // get document reference to first instance of "Angela" found or null
+    const userReference = await getUser(db, "Angela");
+    console.log(userReference);
+
+    if (userReference){
+        // if user was found, retrieve the ACTUAL document's information, not just the reference
+        const angelaInfo = (await getDoc(userReference)).data();
+        console.log(angelaInfo);
+    }
 
     //add a new food to the food collection and output the updated food collection
-
+    
 
     //get a DocumentReference to the new food
 
     
     //add your favorite character to the user collection, add reference above to their "orders", and output the updated collection
+    const newCharacter: User = {
+        address: "China",
+        name: "Po",
+        orders: [],
+        phoneNumber: "1000000000"
+    };
 
+    await addUser(db, newCharacter);
 
     //get your neighbor's user entry
-
+    const neighborRef = await getUser(db, "Angela N");
 
     //remove your neighbor from the user collection :(
+    if (neighborRef){
+        await deleteDoc(neighborRef);
+        console.log("Deleted.");
+    } else {
+        console.log("Not found.");
+    }
 
+    //add a food to someone's order
+    const newFoodRef = await getFood(db, "Buko Pandan");
+    const userRef2 = await getUser(db, "Angela N.");
+
+    if (userRef2 && newFoodRef){
+        await addOrder(userRef2, newFoodRef);
+    }
 
     //output updated user collection
+    const updatedUsers = await getUsers(db);
+    console.log(updatedUsers);
 }
 
 main();
+// npx tsx firebase-example.ts
